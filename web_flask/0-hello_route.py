9e24fc1c7811df03944532ade769644cba11f9ee
Ezejanu <Ezejanu_nno@ymail.com>
2023-11-23 08:56:33 -0800

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+using the option strict_slashes=False
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hello_HBNB", strict_slashes=False)
+def hello_HBNB():
+    """ A method to return hello HBNB """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    # Run the app
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,2 @@
+This is a continuation of our AirBnB clone. This project focuses on the Web framework.
+Using flask.
